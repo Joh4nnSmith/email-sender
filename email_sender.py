2713f9d69d3b3fc07eb48e8dea5487a26c9fb4a6
Johan Smith <j0h4nn.10@gmail.com>
2021-08-03 19:57:46 -0500

    feat: Add class ExcelReader and functions
    Add functions init, load_file, count_rows, count_columns and
    extract_data. Add code in __main__

--- email_sender.py
+++ email_sender.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+# from getpass import getpass
+from os import path
+# import smtplib
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# from decouple import config
+from openpyxl import load_workbook
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class ExcelReader:
+    def __init__(self, path_file):
+        self.path_file = path_file
+        self.workbook = self.load_file()
+        self.n_column = self.count_columns()
+        self.n_rows = self.count_rows()
+
+    def load_file(self):
+        excel_file = load_workbook(self.path_file)
+        return load_workbook(self.path_file).active
+
+    def count_rows(self):
+        for nrow, cell in enumerate(self.workbook['A']):
+            if cell.value is None:
+                return nrow
+                break
+
+    def count_columns(self):
+        return self.workbook.max_column
+
+    def extract_data(self, index):
+        data_student = []
+        for row in self.workbook[index]:
+            data_student.append(row.value)
+        print(data_student[:2], data_student[2:])
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Iniciando ejecución 3')
+    # pss = getpass()
+    # print(pss)
+    file_path = r'C:\Users\ASUS\Desktop\datos.xlsx'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if path.isfile(file_path):
+        file = ExcelReader(file_path)
+        print(file.n_column)
+        print(file.n_rows)
+        for i in range(1, file.n_rows + 1):
+            print(file.extract_data(i))
+    else:
+        print("Error, the file does not exists")
